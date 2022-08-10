f6c28a03bb72c811f23fb39f5546b655b93c4c73
frontierSpaceAlien <yxx6616@autuni.ac.nz>
2022-08-10 19:22:27 +1200

    updated app.js

--- app.js
+++ app.js
@@ -18,19 +18,17 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static('mern-stack-app-frontend/build'));
+
+    app.get("/*", (req, res) => {
+        res.sendFile(path.join(__dirname, "mern-stack-app-frontend", "build", "index.html"));
+    });
+}
 
 // use Routes
 app.use('/api/books', books);
 
 const port = process.env.PORT || 5000;
-
-if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('mern-stack-app-frontend/build'));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "mern-stack-app-frontend", "build", "index.html"));
-    });
-}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
