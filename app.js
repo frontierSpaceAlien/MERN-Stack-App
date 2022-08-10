986ec895e3871fcb23ba75b76df929ec22dea4cc
frontierSpaceAlien <yxx6616@autuni.ac.nz>
2022-08-10 18:45:06 +1200

    Update

--- app.js
+++ app.js
@@ -26,7 +26,11 @@ app.use('/api/books', books);
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('mern-stack-app-frontend/build'));
+    app.use(express.static(path.join(__dirname, '/mern-stack-app-frontend/build')));
+
+    app.get('*', (req,res)=>{
+        res.sendFile(path.join(__dirname, 'mern-stack-app-frontend','build', 'index.html'));
+    })
 }
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
