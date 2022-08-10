ae2eac5201acb9415e367e3d09b11a442c9a06da
frontierSpaceAlien <yxx6616@autuni.ac.nz>
2022-08-10 19:08:02 +1200

    Updated bug

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-const dotenv = require("dotenv").config({path: ".env"});
+const dotenv = require("dotenv");
+dotenv.config();
 
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      process.env.MONGODB_URI,
+      process.env.MONGODB_CONNECTION_STRING,
       {
         useNewUrlParser: true
       }
